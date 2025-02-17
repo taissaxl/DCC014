e17687f96a0690522931e3499789b8b73c4e5e19
makosear <julio.klato@gmail.com>
2025-02-17 15:26:20 -0300

    Adiciona um iterador em lista.h para loops e etc

--- include/lista.h
+++ include/lista.h
@@ -21,6 +21,41 @@ class Lista {
         No<T>* getUltimo();
         bool estaVazia();
         int getTamanho();
+
+        class Iterador {
+            private:
+                No<T>* atual;
+        
+            public:
+                Iterador(No<T>* no) : atual(no) {}
+        
+                // preincremento
+                Iterador& operator++() {
+                    if (atual) {
+                        atual = atual->getProximo();
+                    }
+                    return *this;
+                }
+        
+                // desigualdade
+                bool operator!=(const Iterador& outro) const {
+                    return atual != outro.atual;
+                }
+        
+                // retorna o elemento do iterador
+                T* operator*() const {
+                    return atual->getElemento();
+                }
+            };
+        
+            // Métodos para retornar iteradores
+            Iterador begin() {
+                return Iterador(primeiro);
+            }
+        
+            Iterador end() {
+                return Iterador(nullptr);
+            }
 };
 
 #endif // LISTA_H
